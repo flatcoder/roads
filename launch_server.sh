4af6ad68fa7c6bd69d0217202bc8c54e3908b46e
Gary Rigg <gary.rigg@barefoot.live>
2019-05-09 22:00:53 +0100

    lfs

--- launch_server.sh
+++ launch_server.sh
@@ -4,11 +4,12 @@ python manage.py database upgrade
 if [ ! -e ./csv/data.done ]
 then
     echo "First launch, import..."
+    gzip -d ./csv/data.csv.gz
     python manage.py import_csv -c ./csv/data.csv
     cp ./csv/data.csv ./csv/data.done
     python manage.py backup_json -f ./csv/dbinit.json
+    gzip ./csv/data.csv.gz
 fi
 
-# TEMP! see above.
-python manage.py backup_json -f ./csv/dbinit.json
+#TMP: python manage.py backup_json -f ./csv/dbinit.json
 python manage.py runserver -h 0.0.0.0
